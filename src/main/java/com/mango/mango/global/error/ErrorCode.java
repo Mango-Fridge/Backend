0f9da5bdff4a91d7aca3c846668e4d5d79e4bf7a
Hwangsedong <saedongh5@naver.com>
2025-02-09 16:47:12 +0900

    Feat - User 관련 ErrorCode 정의

--- src/main/java/com/mango/mango/global/error/ErrorCode.java
+++ src/main/java/com/mango/mango/global/error/ErrorCode.java
@@ -16,7 +16,9 @@ public enum ErrorCode {
     INVALID_EMAIL_FORMAT("U001", "올바른 이메일 형식이 아닙니다."),
     DUPLICATE_EMAIL("U002", "이미 가입된 이메일입니다."),
     INVALID_PASSWORD("U003", "비밀번호 형식이 올바르지 않습니다."),
-    REQUIRED_AGREEMENT("U004", "필수 약관에 동의해주세요.");
+    REQUIRED_AGREEMENT("U004", "필수 약관에 동의해주세요."),
+    DUPLICATE_PHONE_NUMBER("U005", "이미 가입된 전화번호 입니다."),
+    DUPLICATE_NICKNAME("U006", "이미 가입된 닉네임 입니다.");
 
     private final String code;
     private final String message;
