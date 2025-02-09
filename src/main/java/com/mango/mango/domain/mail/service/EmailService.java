9a890d0533b104c88c267198c8dbea739ecb1c7e
hamsangjin <rhkstlash@naver.com>
2025-02-09 20:44:13 +0900

    Refactor - EmailController 로직 EmailServiceImpl로 분리

--- src/main/java/com/mango/mango/domain/mail/service/EmailService.java
+++ src/main/java/com/mango/mango/domain/mail/service/EmailService.java
@@ -1,5 +1,9 @@
 package com.mango.mango.domain.mail.service;
 
+import com.mango.mango.domain.mail.dto.request.EmailVerifyRequestDto;
+
 public interface EmailService {
-    void sendVerificationEmail(String email, String code);
+    void sendVerificationEmail(String email);
+
+    boolean confirmCode(EmailVerifyRequestDto requestDto);
 }
