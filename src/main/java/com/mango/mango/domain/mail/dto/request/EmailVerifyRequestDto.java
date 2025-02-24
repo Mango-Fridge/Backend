f02240fac8a45d9fc81a96ea5a87e147950ab36f
Hwangsedong <saedongh5@naver.com>
2025-02-24 22:03:46 +0900

    Feat - mail 삭제

--- src/main/java/com/mango/mango/domain/mail/dto/request/EmailVerifyRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.mango.mango.domain.mail.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class EmailVerifyRequestDto {
-    private String email;
-    private String code;
-}
--- src/main/java/com/mango/mango/domain/mail/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.mango.mango.domain.mail.service;
-
-import com.mango.mango.domain.mail.dto.request.EmailVerifyRequestDto;
-
-public interface EmailService {
-    void sendVerificationEmail(String email);
-
-    boolean confirmCode(EmailVerifyRequestDto requestDto);
-}
