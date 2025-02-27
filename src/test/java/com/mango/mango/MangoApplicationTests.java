fa49f8e134f1e875b77ae3660531324bc2dd9f2c
haesookim <haesookim0106@plateer.com>
2025-02-27 13:35:32 +0900

    데이터 베이스 환경변수 추가 및 자바 빌드 오류 해결

--- src/test/java/com/mango/mango/MangoApplicationTests.java
+++ src/test/java/com/mango/mango/MangoApplicationTests.java
@@ -3,7 +3,7 @@ package com.mango.mango;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=MangoApplicationTests.class)
 class MangoApplicationTests {
 
 	@Test
