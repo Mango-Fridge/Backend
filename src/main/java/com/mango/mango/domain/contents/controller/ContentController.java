9969dd036462011ba862042b76b9c07ea5eb748e
hamsangjin <rhkstlash@naver.com>
2025-03-08 16:50:14 +0900

    [Docs]: Swagger에 API 기능 명시

--- src/main/java/com/mango/mango/domain/contents/controller/ContentController.java
+++ src/main/java/com/mango/mango/domain/contents/controller/ContentController.java
@@ -4,6 +4,7 @@ import com.mango.mango.domain.contents.dto.request.ContentRequestDto;
 import com.mango.mango.domain.contents.dto.response.ContentResponseDto;
 import com.mango.mango.domain.contents.service.ContentService;
 import com.mango.mango.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +17,19 @@ import java.util.List;
 public class ContentController {
     private final ContentService contentService;
 
-    // 메인화면 냉장고 그룹에 따른 내용물 노출
+    @Operation(summary = "[3] 메인화면 - 메인화면 냉장고 그룹에 따른 내용물 노출")
     @GetMapping("/group/{groupId}")
     public ResponseEntity<ApiResponse<List<ContentResponseDto>>> getContentsByGroupId(@PathVariable Long groupId) {
         return contentService.getContentsByGroupId(groupId);
     }
 
-    // 메인화면 냉장고 내용물 수량 조절
+    @Operation(summary = "[3] 메인화면 - 메인화면 냉장고 내용물 수량 조절")
     @PutMapping("/update")
     public ResponseEntity<ApiResponse<?>> updateContents(@RequestBody ContentRequestDto req) {
         return contentService.updateContentCounts(req);
     }
 
-    // 물품 상세 정보
+    @Operation(summary = "[3] 메인화면 - 물품 상세 정보")
     @GetMapping("/{contentId}")
     public ResponseEntity<ApiResponse<ContentResponseDto>> getContentDetail(@PathVariable Long contentId) {
         return contentService.getContentDetail(contentId);
--- src/main/java/com/mango/mango/domain/groups/controller/GroupController.java
+++ src/main/java/com/mango/mango/domain/groups/controller/GroupController.java
@@ -3,6 +3,7 @@ package com.mango.mango.domain.groups.controller;
 import com.mango.mango.domain.groups.dto.response.GroupResponseDto;
 import com.mango.mango.domain.groups.service.GroupService;
 import com.mango.mango.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ import java.util.List;
 public class GroupController {
     private final GroupService groupService;
 
-    // 냉장고 그룹 불러오기
+    @Operation(summary = "[3] 메인화면 - 냉장고 그룹 불러오기")
     @GetMapping("/user/{userId}")
     public ResponseEntity<ApiResponse<List<GroupResponseDto>>> getGroupsByUserId(@PathVariable Long userId) {
         return groupService.getGroupsByUserId(userId);
