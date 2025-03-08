fb14ab8ec80407e949ce12f2b8312dce9279b672
Hwangsedong <saedongh5@naver.com>
2025-03-09 02:35:59 +0900

    Config : Swagger Config 수정

--- src/main/java/com/mango/mango/config/swagger/SwaggerConfig.java
+++ src/main/java/com/mango/mango/config/swagger/SwaggerConfig.java
@@ -1,19 +1,32 @@
 package com.mango.mango.config.swagger;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Mango API")
-                        .description("Mango 프로젝트 API 문서")
-                        .version("v1.0.0"));
+            .info(new Info()
+                .title("Mango API")
+                .version("1.0"))
+            .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("BearerAuth",
+                    new SecurityScheme()
+                        .name("BearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                )
+            );
     }
 }
